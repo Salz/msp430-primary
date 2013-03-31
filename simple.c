3aa8fd0cdd919f20d770cbb4eb8f1a75db6c0a8d
Ingo Saitz <Ingo.Saitz@stud.uni-hannover.de>
2013-03-31 16:30:09 +0200

    Build, Flash notes and simple as dice

--- simple.c
+++ simple.c
@@ -1,5 +1,5 @@
 // See http://homepages.ius.edu/rwisman/C335/html/Syllabus.htm
-//
+
 #include <msp430.h>
 #include <legacymsp430.h>
 
@@ -7,8 +7,11 @@ int main(void) {
 	volatile int c, i;
 
 	WDTCTL = WDTPW + WDTHOLD; // Stop watchdoc timer
-	P1DIR |= 1<<0; // Set P1.0 (red LED?) to output
+	P1DIR |= 1<<0; // Set P1.0 (red LED) to output
+	P1OUT &= ~(1<<0); // Turn LED off
+
 	P1DIR |= 1<<6; // Set P1.6 (green LED) to output
+	P1OUT &= ~(1<<6); // Turn LED off
 
 	P1REN |=  1<<3; // Enable pull-up resistor
 	P1DIR &= ~(1<<3); // Set P1.3 (left switch) to input
@@ -17,15 +20,15 @@ int main(void) {
 	c = 0;
 	while (1) {
 		// Toggle green LED
-		P1OUT ^= 1<<6; // Toggle P1.6
-		// Toggle red LED via switch
-		if (P1IN & 1<<3) {
-			P1OUT &= ~(1<<0);
-		} else {
-			P1OUT |= 1<<0;
+		if (c%5 == 0) {
+			P1OUT ^= 1<<6; // Toggle P1.6
+		}
+		// Toggle red LED if switch pressed
+		if ((P1IN & 1<<3) == 0) {
+			P1OUT ^= 1<<0;
 		}
 		// Delay
-		for (i = 0, c+=5; i < c; i++);
+		for (i = 0, c++; i < c; i++);
 	}
 }
 
