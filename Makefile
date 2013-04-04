73aabdf2893e81ac06d1ee7220430fdfc221fc96
Ingo Saitz <Ingo.Saitz@stud.uni-hannover.de>
2013-04-04 13:24:05 +0200

    simple low power example

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC=msp430-gcc
 CFLAGS=-Os -Wall -g -mmcu=msp430g2553
 
-BINARIES=simple binout pushcount
+BINARIES=simple binout pushcount lowpowled
 
 all: $(BINARIES)
 
---
+++ lowpowled.c
@@ -0,0 +1,33 @@
+// toggle LED1 while being in low power mode
+
+#include <msp430.h>
+#include <legacymsp430.h>
+
+#define LED1 (1<<0)
+#define LED2 (1<<6)
+
+#define BTN (1<<3)
+
+int main(void) {
+	WDTCTL = WDTPW + WDTHOLD; // Stop watchdog timer
+
+	P1DIR |=  LED1; // Set P1.0 (red LED) to output
+	P1OUT &= ~LED1; // Turn LED off
+
+	P1SEL &= ~BTN;	// Select Button
+	P1DIR &= ~BTN;	// Select as Input (in = 0, out = 1)
+	P1REN |=  BTN;	// Pull-Up Resistor
+
+	P1IE  |=  BTN;	// Generate interrupt
+	P1IFG &= ~BTN;	// Clear Interrupt
+
+	_BIS_SR(LPM3_bits + GIE); // Enable low power (1uA) and interrupts
+
+	for(;;);
+}
+
+#pragma vector=PORT1_VECTOR
+__interrupt void toggle(void) {
+	P1IFG &= ~BTN;	// Clear Interrupt
+	P1OUT ^= LED1;	// Toggle LED
+}
