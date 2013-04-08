2ddfd1f6d8f47a813af583843fec89ddb69fef32
Ingo Saitz <Ingo.Saitz@stud.uni-hannover.de>
2013-04-08 17:33:46 +0200

    nice playground for tail recursion with -Os

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC=msp430-gcc
 CFLAGS=-Os -Wall -g -mmcu=msp430g2553
 
-BINARIES=simple binout pushcount lowpowled
+BINARIES=simple binout pushcount lowpowled bad_recursion
 
 all: $(BINARIES)
 
---
+++ bad_recursion.c
@@ -0,0 +1,26 @@
+#include <msp430.h>
+
+#define LED1 (1<<0)
+void blink();
+
+int main() {
+	WDTCTL = WDTPW | WDTHOLD;
+
+	P1OUT &= ~LED1
+	P1DIR |= LED1;
+
+	blink();
+	return 0;
+}
+
+// gcc -Os does tail recursion, so this function does not actually overflow
+// Try compiling with -O0 or with the last comment removed.
+void blink() {
+	volatile unsigned i;
+
+	P1OUT ^= LED1;
+	for(i = 0; i < 3000; i++);
+	blink();
+
+	//for(i = 0; i < 3000; i++);
+}
